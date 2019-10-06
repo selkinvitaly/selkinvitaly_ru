d4baf5b0aa9a4a01bea5125083da0757ba7fafe9
Selkin Vitaly <contact@selkinvitaly.ru>
2019-10-06 23:22:18 +0300

    updated content

--- client/js/index.js
+++ client/js/index.js
@@ -1,18 +1,18 @@
-import NegativeTopNav from './classes/NegativeTopNav';
+// import NegativeTopNav from './classes/NegativeTopNav';
 import GoToScroll from './classes/GoToScroll';
 import AdaptiveNav from './classes/AdaptiveNav';
-import ViewProject from './classes/ViewProject';
+// import ViewProject from './classes/ViewProject';
 import SendMail from './classes/SendMail';
 
 
-new NegativeTopNav({
-    elems: {
-        topNav: document.querySelector('.g-top-nav')
-    },
-    classes: {
-        negative: 'g-top-nav_negative'
-    }
-});
+// new NegativeTopNav({
+//     elems: {
+//         topNav: document.querySelector('.g-top-nav')
+//     },
+//     classes: {
+//         negative: 'g-top-nav_negative'
+//     }
+// });
 
 new GoToScroll({
     elems: {
@@ -34,18 +34,18 @@ new AdaptiveNav({
     }
 });
 
-new ViewProject({
-    elems: {
-        container: document.querySelector('.g-projects')
-    },
-    classes: {
-        closeBtn: 'g-project-view-close',
-        closed: 'g-project-view_closed',
-        modalWindow: 'g-project-view-win',
-        images: 'g-project-view-gal-images img',
-        gallery: 'blueimp-gallery'
-    }
-});
+// new ViewProject({
+//     elems: {
+//         container: document.querySelector('.g-projects')
+//     },
+//     classes: {
+//         closeBtn: 'g-project-view-close',
+//         closed: 'g-project-view_closed',
+//         modalWindow: 'g-project-view-win',
+//         images: 'g-project-view-gal-images img',
+//         gallery: 'blueimp-gallery'
+//     }
+// });
 
 new SendMail({
     elems: {
